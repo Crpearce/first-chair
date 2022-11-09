dac28a0fcf2cac868f9373e3256893f0e265018e
Colby Pearce <crpearce892@gmail.com>
2022-11-08 23:22:33 -0700

    initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { 
+
+     }
+  }
+  render() { 
+    return ( 
+      <h1>test</h1>
+     )
+  }
+}
+ 
+export default App;
