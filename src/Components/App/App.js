c5977781fe87f6b028d379b83597e0e6b125a058
Colby Pearce <crpearce892@gmail.com>
2022-11-09 19:33:29 -0700

    update app component with router, add api call and componentDidMount for fetch

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,44 @@
-
+import React, { Component } from 'react'
+import Navigation from '../Navigation/Navigation';
+import Resorts from '../Resorts/Resorts';
+import SingleResort from '../SingleResort/SingleResort';
+import { Route } from 'react-router-dom';
+import { fetchAllResorts } from '../../api';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = { 
+        resorts: [],
+        error: '',
+     };
+  };
 
-     }
+  componentDidMount = async () => {
+    try {
+      const resortList = await fetchAllResorts();
+      const data = await resortList.json();
+      this.setState({ resorts: data.resorts });
+    } catch {
+      this.setState({ error: "Sorry, we are having trouble loading the data. Please try again or check back later!"});
+    };
+  };
+
+  displayResortDetails = (id) => {
+    const currentResort = this.state.resorts.find(resort => resort.id === id)
+    this.setState({ singleResort: currentResort })
   }
+
   render() { 
     return ( 
-      <h1>test </h1>
+      <main className='App'>
+        {/* <Route path='/' render={() => <Navigation />} /> */}
+        <Route exact path='/' render={() => <Resorts resorts={this.state.resorts} />} />
+        {/* <Route path='/:resortId' render={({ match }) => <SingleResort resortId={match.params.resortId}/>} /> */}
+      </main>
      )
-  }
-}
+  };
+};
  
 export default App;
---
+++ src/api.js
@@ -0,0 +1,3 @@
+export const fetchAllResorts = async () => {
+    return await fetch("https://first-chair-api.herokuapp.com/api/v1/resorts");
+  };
